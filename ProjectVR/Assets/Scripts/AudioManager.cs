0210b980c481028e4154aba7757057ae19612981
Kevin Jaramillo C <kevinjaramillocorrea@gmail.com>
2018-05-16 12:26:33 -0500

    Add the project

---
+++ ProjectVR/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource ambientAudio;
+    public AudioSource animalSounds;
+	
+	void Start ()
+    {
+        ambientAudio.Play();
+        animalSounds.Play();
+	}
+}
---
+++ ProjectVR/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    [Header("User Interface")]
+    public GameObject panel;
+    //[Header("Game Manager")]
+    //public GameObject sceneLogic;
+
+    private void Update()
+    {
+        StartGameplay();
+    }
+
+    public void StartGameplay()
+    {
+        panel.SetActive(false);
+        //sceneLogic.SetActive(true);
+    }
+}
